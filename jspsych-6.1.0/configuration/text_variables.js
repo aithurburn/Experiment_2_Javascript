a4323a27f572f8969fba22e79ff42f3146b07959
ITHURBURN Andrew <s1932788@ed.ac.uk>
2020-08-25 17:29:57 +0100

    New Files

--- jspsych-6.1.0/configuration/text_variables.js
+++ jspsych-6.1.0/configuration/text_variables.js
@@ -1,58 +1,59 @@
-var spiteOptions =
-  "1 (Strongly Disagree)', '2 (Slightly Disagree)', '3 (Neither Agree nor Disagree)', '4 (Slightly Agree)', '5 (Strongly Agree)";
-var sessOptions = "1 (Strongly Disagree)', '2 (Slightly Disagree)', '3 (Neither Agree nor Disagree)', '4 (Slightly Agree)', '5 (Strongly Agree)";
-var justificationOptions = "";
 var ageStimulus = "What is your age?";
 var genderStimulus = "What gender do you most closely identify with?";
 var genderOptions =
-  "Female (1)Male (2)Gender Non-Binary (3)Trans Male (4)Trans Female (5)Prefer not to respond (6)";
+  "Female (1) Male (2) Gender Non-Binary (3) Trans Male (4) Trans Female (5) Prefer not to respond (6) ";
 var ethnicityStimulus = "What ethnicity do you most closely identify with?";
 var ethnicityOptions =
-  "hite (1) Mixed or Multiple ethnic groups (2) Asian or 66666666666655555Asian Scottish or Asian British (4) African (5) Caribbean or Black (6) Arab (7) Other ethnic group (8)";
+  "White (1)  Mixed or Multiple ethnic groups (2)  Asian or Asian Scottish or Asian British (4) African (5) Caribbean or Black (6) Arab (7) Other ethnic group (8) ";
 var ethnicOriginStimulus =
   "What best describes your ethnic origin or background?";
 var ethnicOriginOptions =
-  "Scottish (1)English (2)European (3)Latin American (4)Asian (5)Arab (6)African (7)Other (8)";
+  "Scottish (1) English (2) European (3) Latin American (4) Asian (5) Arab (6) African (7) Other (8) ";
 var educationStimulus =
   "What is the highest level of education you have attained so far?";
 var educationOptions =
-  "Primary School (1)GCSEs or Equivalent (2)A-Levels or Equivalent (3)University Undergraduate Program (4)University Post-Graduate Program (5)Doctoral Degree (6)Prefer not to answer (7)";
-var spiteOne = "SpiteOne";
-var spiteTwo = "SpiteTwo";
-var spiteThree = "SpiteThree";
-var spiteFour = "SpiteFour";
-var spiteFive = "SpiteFive";
-var spiteSix = "SpiteSix";
-var spiteSeven = "SpiteSeven";
-var spiteEight = "SpiteEight";
-var spiteNine = "SpiteNine";
-var spiteTen = "SpiteTen";
-var spiteEleven = "SpiteEleven";
-var spiteTwelve = "SpiteTwelve";
-var spiteThirteen = "SpiteThirteen";
-var spiteFourteen = "SpiteFourteen";
-var spiteFifteen = "SpiteFifteen";
-var spiteSixteen = "SpiteSixteen";
-var spiteSeventeen = "SpiteSeventeen";
-var sessOne = "sessOne";
-var sessTwo = "sessTwo";
-var sessThree = "sessThree";
-var sessFour = "sessFour";
-var sessFive = "SessFive";
-var sessSix = "sessSix";
-var sessSeven = "sessSeven";
-var sessEight = "sessEight";
-var sessNine = "sessNine";
-var sessTen = "sessTen";
-var sessEleven = "sessEleven";
-var sessTwelve = "sessTwelve";
-var sessThirteen = "sessThirteen";
-var sessFourteen = "sessFourteen";
-var sessFifteen = "sessFifteen";
-var sessSixteen = "sessSixteen";
-var sessSeventeen = "sessSeventeen";
-var sessEighteen = "sessEighteen";
-var sessNineteen = "sessNineteen";
-var sessTwenty = "sessTwenty";
-var sessTwentyOne = "sessTwentyOne";
-var sessTwentyThree = "sessTwentyThree";
+  "Primary School (1) GCSEs or Equivalent (2) A-Levels or Equivalent (3) University Undergraduate Program (4) University Post-Graduate Program (5) Doctoral Degree (6) Prefer not to answer (7) ";
+var spiteOptions =
+  "1 (Strongly Disagree) ', '2 (Slightly Disagree) ', '3 (Neither Agree nor Disagree) ', '4 (Slightly Agree) ', '5 (Strongly Agree) ";
+var sessOptions =
+  "1 (Strongly Disagree) ', '2 (Slightly Disagree) ', '3 (Neither Agree nor Disagree) ', '4 (Slightly Agree) ', '5 (Strongly Agree) ";
+var spiteOne = "Spitefulness One";
+var spiteTwo = "Spitefulness Two";
+var spiteThree = "Spitefulness Three";
+var spiteFour = "Spitefulness Four";
+var spiteFive = "Spitefulness Five";
+var spiteSix = "Spitefulness Six";
+var spiteSeven = "Spitefulness Seven";
+var spiteEight = "Spitefulness Eight";
+var spiteNine = "Spitefulness Nine";
+var spiteTen = "Spitefulness Ten";
+var spiteEleven = "Spitefulness Eleven";
+var spiteTwelve = "Spitefulness Twelve";
+var spiteThirteen = "Spitefulness Thirteen";
+var spiteFourteen = "Spitefulness Fourteen";
+var spiteFifteen = "Spitefulness Fifteen";
+var spiteSixteen = "Spitefulness Sixteen";
+var spiteSeventeen = "Spitefulness Seventeen";
+var sessOne = "sexual questionnaire One";
+var sessTwo = "sexual questionnaire Two";
+var sessThree = "sexual questionnaire Three";
+var sessFour = "sexual questionnaire Four";
+var sessFive = "sexual questionnaire Five";
+var sessSix = "sexual questionnaire Six";
+var sessSeven = "sexual questionnaire Seven";
+var sessEight = "sexual questionnaire Eight";
+var sessNine = "sexual questionnaire Nine";
+var sessTen = "sexual questionnaire Ten";
+var sessEleven = "sexual questionnaire Eleven";
+var sessTwelve = "sexual questionnaire Twelve";
+var sessThirteen = "sexual questionnaire Thirteen";
+var sessFourteen = "sexual questionnaire Fourteen";
+var sexualFifteen = "sexual questionnaire Fifteen";
+var sexualSixteen = "sexual questionnaire Sixteen";
+var sexualSeventeen = "sexual questionnaire Seventeen";
+var sexualEighteen = "sexual questionnaire Eighteen";
+var sexualNineteen = "sexual questionnaire Nineteen";
+var sexualTwenty = "sexual questionnaire Twenty";
+var sexualTwentyOne = "sexual questionnaire TwentyOne";
+var sexualTwentyTwo = "sexual questionnaire TwentyTwo";
+var sexualTwentyThree = "sexual questionnaire TwentyThree";
